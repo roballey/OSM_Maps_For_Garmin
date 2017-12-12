e4bad1b273a248fe9c17f4db582e0753f98e088a
alleyr <Rob.Alley@fphcare.co.nz>
2017-12-13 10:04:14 +1300

    Add messages whilst processing data

--- build/build_contours.sh
+++ build/build_contours.sh
@@ -1,7 +1,9 @@
 # Build PBF files of contour data from SRTM data. The SRTM data will be downloaded into a directory named hgt if it is not already there
-# This script is copied from: https://wiki.openstreetmap.org/wiki/OSM_Map_on_Garmin/Contours_using_phygtmap
+# This script is based on: https://wiki.openstreetmap.org/wiki/OSM_Map_on_Garmin/Contours_using_phygtmap
 pushd work/contours
+echo "Removing existing contour pbf files..."
 rm *.pbf
+echo "Generating contour pbf files from SRTM data..."
 phyghtmap \
  --step=50 \
  --line-cat=400,100 \
