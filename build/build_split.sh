bbe754ccf74abbc0d8a1a1dd9edec77163e7a8fd
alleyr <Rob.Alley@fphcare.co.nz>
2017-12-13 13:32:08 +1300

    Replace build_split.sh with split.sh build script

--- build/build_split.sh
+++
@@ -1,5 +0,0 @@
-#
-# Split single PBF file into multiple files for processing via mkgmap
-#
-rm work/osmsplitmaps/*.osm.pbf
-java -Xmx1024m -jar tools/splitter-r580/splitter.jar input/oceania_nz_ni.pbf --output-dir=work/osmsplitmaps/
