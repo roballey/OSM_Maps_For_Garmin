d292cc830f4d7928c2be8c9ec3dc6d77b7d5997b
roballey <alley.rob@gmail.com>
2017-12-13 21:04:55 +1300

    Remove seperate script to generate tdb file now that it built in to gmapsupp.sh

--- build/build_tdb.sh
+++
@@ -1,5 +0,0 @@
-#
-# Build the tdb file and copy in the typ file both required if using QLANDKARTE to view maps on the PC
-#
-java -jar tools/mkgmap-r3834/mkgmap.jar --tdbfile --output-dir=maps/nonroute/ maps/nonroute/oceania_nz_ni.img 
-cp build/offroad.typ maps/nonroute/offroad.typ
