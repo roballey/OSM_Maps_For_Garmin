3c1eaa294517a8a0877acd558b0d7759a331a01d
roballey <alley.rob@gmail.com>
2018-04-01 13:40:08 +1200

    Replace individual build scripts with single script that accepts arguments

---
+++ build/install705.sh
@@ -0,0 +1,10 @@
+# Set the name of the region being generated
+region="oceania_nz_ni"
+
+# Set the style that is to be applied when converting from OSM to Garmin
+style="route"
+
+# Setup path to output files
+output_dir="maps/${style}/${region}"
+
+cp ${output_dir}/gmapsupp.img /media/rob/92E4-4983/GARMIN/gmapbmap.img
