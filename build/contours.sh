e36dee5595d34e99ae9abeb6569b2218ed5fb05a
Rob Alley <alley.rob@gmail.com>
2020-02-12 22:18:11 +1300

    Get contours script working

--- build/contours.sh
+++ build/contours.sh
@@ -1,6 +1,8 @@
-#! /bin/sh
-# Build PBF files of contour lines from SRTM data. The SRTM data will be downloaded into the hgtdir directory if it is not already there
+#! /bin/bash
+# Build PBF files of contour lines from Viewfinder data. The Viewfinder data will be downloaded into the hgtdir directory if it is not already there
 # This script is based on: https://wiki.openstreetmap.org/wiki/OSM_Map_on_Garmin/Contours_using_phygtmap
+#
+# NOTE: OSM data must have been downloaded and split before running this script as it relies on the ploygon file written by the split script (build/split.sh)
 
 # Set the name of the region being generated
 region="oceania_nz_ni"
@@ -15,8 +17,9 @@ pushd ${contour_dir}
 echo "Removing existing contour pbf files..."
 rm *.pbf
 
-echo "Generating contour pbf files from SRTM data..."
+echo "Generating contour pbf files from Viewfinder data..."
 phyghtmap \
+ --source=view1,view3 \
  --step=20 \
  --line-cat=200,100 \
  --polygon=../../osmsplitmaps/${region}/areas.poly \
