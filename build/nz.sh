868c785c6557a108d56301fedbe9a409ed1e4954
Rob Alley <alley.rob@gmail.com>
2020-06-13 10:17:39 +1200

    Also build routeable images

--- build/nz.sh
+++ build/nz.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 # Download all of NZ pbf
-#echo "Downloading ..."
-#wget https://download.geofabrik.de/australia-oceania/new-zealand-latest.osm.pbf --output-document=input/oceania_nz.pbf
+echo "Downloading ..."
+wget https://download.geofabrik.de/australia-oceania/new-zealand-latest.osm.pbf --output-document=input/oceania_nz.pbf
 
 # Split input file, cropping to only NI
 echo "Splitting ..."
@@ -12,7 +12,7 @@ build/split.sh -r oceania_nz_si -p oceania_nz_si.poly -i oceania_nz.pbf
 # Build Garmin img files from split PBFs
 echo "Building image files ..."
 build/map.sh -t nonroute -s nonroute -r oceania_nz_ni
-#build/map.sh -t route -s route -r oceania_nz_ni
+build/map.sh -t route -s route -r oceania_nz_ni
 
 build/map.sh -t nonroute -s nonroute -r oceania_nz_si
-#build/map.sh -t route -s route -r oceania_nz_si
+build/map.sh -t route -s route -r oceania_nz_si
