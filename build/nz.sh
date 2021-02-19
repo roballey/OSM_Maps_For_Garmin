d06ad4d1d0229acf1529847833450f83203fdff3
Rob Alley <alley.rob@gmail.com>
2021-02-19 21:18:09 +1300

    Build images for whole of NZ

--- build/nz.sh
+++ build/nz.sh
@@ -4,17 +4,22 @@
 echo "Downloading ..."
 wget https://download.geofabrik.de/australia-oceania/new-zealand-latest.osm.pbf --output-document=input/oceania_nz.pbf
 
-# Split input file, cropping to only NI
+# Split input file, for whole of NZ and also cropped to only NI and SI
 echo "Splitting ..."
-build/split.sh -r oceania_nz_ni -p oceania_nz_ni.poly -i oceania_nz.pbf
-build/split.sh -r oceania_nz_si -p oceania_nz_si.poly -i oceania_nz.pbf
+build/split2.sh -r oceania_nz -p oceania_nz.poly -i oceania_nz.pbf
+build/split2.sh -r oceania_nz_ni -p oceania_nz_ni.poly -i oceania_nz.pbf
+build/split2.sh -r oceania_nz_si -p oceania_nz_si.poly -i oceania_nz.pbf
 
 # Build Garmin img files from split PBFs
 echo "Building image files ..."
+echo "   Oceania NZ ..."
+build/map2.sh -t nonroute -s nonroute -r oceania_nz
+build/map2.sh -t route -s route -r oceania_nz
+
 echo "   Oceania NZ NI ..."
-build/map.sh -t nonroute -s nonroute -r oceania_nz_ni
-build/map.sh -t route -s route -r oceania_nz_ni
+build/map2.sh -t nonroute -s nonroute -r oceania_nz_ni
+build/map2.sh -t route -s route -r oceania_nz_ni
 
 echo "   Oceania NZ SI ..."
-build/map.sh -t nonroute -s nonroute -r oceania_nz_si
-build/map.sh -t route -s route -r oceania_nz_si
+build/map2.sh -t nonroute -s nonroute -r oceania_nz_si
+build/map2.sh -t route -s route -r oceania_nz_si
