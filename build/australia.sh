a68ce2e08de3024f51b2b1d58acc4a4cb054ec82
Rob Alley <rob.alley@gmail.com>
2023-06-10 21:27:06 +1200

    Add scripts to build Aus and RSA maps

---
+++ build/australia.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# Download latest OSM data and build Garmin GPS supplementary map images for South Africa
+
+# Download pbf
+echo "Downloading ..."
+wget https://download.geofabrik.de/australia-oceania/australia-latest.osm.pbf --output-document=input/australia.pbf
+
+# Split input file
+echo "--- Splitting ..."
+build/split.sh -r australia -i australia.pbf
+
+# Build Garmin img files from split PBFs
+echo "--- Building image file ..."
+echo "---    Australia ..."
+build/map.sh -t route -s route -r australia
+
