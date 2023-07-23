b1041b65ba6325e5562e2f5d8f9cbba544f40c73
Rob Alley <rob.alley@gmail.com>
2023-07-23 15:50:12 +1200

    Update 705-1 install script

--- build/install705-1.sh
+++ build/install705-1.sh
@@ -5,4 +5,10 @@ style="route"
 output_dir="maps/${style}/${region}"
 
 # Copy to supplementary map file on Garmin SD-Card of Edge 705-1
-cp ${output_dir}/gmapsupp.img /media/rob/92E4-4983/GARMIN/gmapsupp.img
+#cp ${output_dir}/gmapsupp.img /media/rob/92E4-4983/GARMIN/gmapsupp.img
+# Updated to list file sizes and use Acer Aspire 5 Linut Mint mount point
+echo "Installing ${output_dir} ..."
+echo "Before " `ls -lh /media/rob/HOCO64/Garmin/gmapsupp.img`
+cp ${output_dir}/gmapsupp.img /media/rob/HOCO64/Garmin/gmapsupp.img
+echo "After " `ls -lh /media/rob/HOCO64/Garmin/gmapsupp.img`
+
