495ecd9732d9c514e9946ac585ab64fc3b4cb9fa
Rob Alley <rob.alley@gmail.com>
2024-06-29 10:52:15 +1200

    Improve display on 955 routeable maps:   Increase size of tree in woods pattern   Dont draw icon for pitches, recreation grounds, sports tracks   Update colours for wood, scrub, park, playground, pitch   Draw pitches over parks   Make paths and footways brighter
    Improve install script messages

--- build/install955.sh
+++ build/install955.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 # Install Garmin map file on 955 mounted with MTP
 # Just defaults to NZ wide routeable map for now
-# FIXME: ls -lh reports size of 0, seems ok in Nautilus file explorer
+# FIXME: ls -lh sometimes reports size of 0, seems ok in Nautilus file explorer
 
 # Default the region and type of image to install
 region="oceania_nz"
@@ -13,9 +13,9 @@ dest="/run/user/1000/gvfs/mtp:host=091e_4fb8_0000cbd4bc05/Internal Storage/GARMI
 # Build source map directory
 map_dir="maps/${style}/${region}"
 
-echo "Installing ${map_dir} to ${dest} ..."
+echo "Installing from '${map_dir}' to '${dest}' ..."
 echo "(takes ~ 15 seconds)..."
 gio copy ${map_dir}/gmapsupp.img "${dest}"
 echo "On Garmin:"
 ls -lh "${dest}"
-echo "(Size reports as 0 for some reason)"
+echo "(Size reports as 0 sometimes)"
